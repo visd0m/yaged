494ee0425419b55dd5eb51a6462df37233f3d548
Domenico Visconti <domenico.visconti@prima.it>
2020-10-16 19:42:20 +0200

    first commit

---
+++ src/decoder/steps/screen_descriptor.rs
@@ -0,0 +1,29 @@
+use crate::decoder::steps::nth_bit;
+use crate::types::ScreenDescriptor;
+
+pub fn decode(bytes: &Vec<u8>, cursor: usize) -> (ScreenDescriptor, usize) {
+    let to_index = cursor + 7;
+    let screen_descriptor = &bytes[cursor..to_index];
+
+    let screen_width = ((screen_descriptor[1] as u16) << 8) | screen_descriptor[0] as u16;
+    let screen_height = ((screen_descriptor[3] as u16) << 8) | screen_descriptor[2] as u16;
+
+    let flags: u8 = screen_descriptor[4];
+    let m: bool = nth_bit(flags, 7);
+    let cr: u8 = (flags << 1) >> 5;
+    let pixel: u8 = (flags << 5) >> 5;
+
+    let background: u8 = screen_descriptor[5];
+
+    (
+        ScreenDescriptor {
+            width: screen_width,
+            height: screen_height,
+            m,
+            cr,
+            pixel,
+            background,
+        },
+        to_index,
+    )
+}
